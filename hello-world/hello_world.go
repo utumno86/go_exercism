8e63a76bdb0ec500c70ee3558f1487f1d223c39e
Michael Nash <michael@spatialnetworks.com>
2019-05-24 15:05:39 -0400

    updated hello-world

--- hello-world/hello_world.go
+++ hello-world/hello_world.go
@@ -1,7 +1,5 @@
-// This package is intended to produce a Hello World output.
+// Package greeting is intended to produce a Hello World output.
 package greeting
-
-const testVersion = 4
 
 // HelloWorld produces a Hello World string
 func HelloWorld() string {
