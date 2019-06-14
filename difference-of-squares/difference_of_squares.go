cdf776aff80b26bb14d9672d641c570a11167fd1
Michael Nash <michael@spatialnetworks.com>
2019-06-14 12:43:30 -0400

    updated difference of squares

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,23 +1,14 @@
 package diffsquares
 
-import "math"
-
 //SquareOfSum returns the square of the sum of natural numbers up to the input
 func SquareOfSum(num int) int {
-	sum := 0
-	for i := 1; i <= num; i++ {
-		sum = sum + i
-	}
-	return int(math.Pow(float64(sum), 2))
+	n := ((num * (num + 1)) / 2)
+	return n * n
 }
 
 //SumOfSquares returns the sum of the squares of natural numbers up to the input
 func SumOfSquares(num int) int {
-	sum := 0
-	for i := 1; i <= num; i++ {
-		sum = sum + int(math.Pow(float64(i), 2))
-	}
-	return sum
+	return (num * (num + 1) * ((2 * num) + 1) / 6)
 }
 
 //Difference returns the differences between Square of Sum and Sum of Squares
